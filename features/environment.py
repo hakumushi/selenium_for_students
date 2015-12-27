546c01060b9c55728be841ab7e5581b054a031a2
mushi720 <pr.francaf@gmail.com>
2015-12-27 00:17:40 -0300

    environment modification

--- features/environment.py
+++ features/environment.py
@@ -4,11 +4,11 @@ __author__ = 'Paulo Franca'
 from selenium import webdriver
 import os
 
-def before_feature(context, feature):
+def before_all(context):
     context.driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.realpath(__file__)) + "/resources/chromedriver")
     #context.driver = webdriver.Firefox()
     context.driver.implicitly_wait(30)
     context.driver.maximize_window()
 
-def after_feature(context, feature):
+def after_all(context):
     context.driver.close()
